94bec5cad902e73897811e4850693aa863492b09
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-17 00:05:18 +0545

    Get all post data

--- src/screen/Read.jsx
+++ src/screen/Read.jsx
@@ -4,9 +4,25 @@ import { useGetAllDataQuery } from "../redux/api/api"
 function Read(){
     const { isLoading, isError, data, isSuccess } = useGetAllDataQuery({})
     console.log(data)
+    
     return <>
         <NavBar/>
         <h3>Posts</h3>
+        {
+            isLoading &&
+            <div>Loading...</div>
+        }
+        {
+            isSuccess &&
+            data.map((item, index)=><div className="cards" key={index}>
+                <div><b>{item.title}</b></div>
+                <div>{item.body}</div>
+            </div>)
+        }
+        {
+            isError && 
+            <div>Error</div>
+        }
     </>
 }
 
