5c2d25c74b8e3f5ca72fcb42393bb74da5f2c0d0
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-16 23:29:33 +0545

    React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import { store } from './redux/store/store.jsx';
 import { Provider } from 'react-redux';
+import AppRouter from './router/router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <AppRouter />
   </Provider>,
 )
