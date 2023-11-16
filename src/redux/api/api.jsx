05af69f25cb485cb36e9f1aaa9f37594a6453de9
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-16 22:45:06 +0545

    API created

---
+++ src/redux/api/api.jsx
@@ -0,0 +1,35 @@
+import {createApi} from "@reduxjs/toolkit/query/react";
+import { axios_instance } from "../../axios/instance";
+
+const api = createApi({
+    reducerPath:"api",
+    baseQuery: async(args, api, extraOptions)=>{
+        // args -> includes the query parameters, request body, and other 
+        //         information required for the API request.
+
+        // api -> includes information about the base URL endpoints and 
+        //        other configurations.
+
+        // extraOptions -> it is an object that includes additional options 
+        //                 passed to the baseQuery function.
+        //              -> it provides extra information or configurations.
+        const { endpoint, query, body } = args;
+        try{
+            const response = await axios_instance({
+                url: `${endpoint}`,
+                method: (extraOptions && extraOptions.method)||'get',
+                // parameter: defines the behaviour of query.
+                params: query,
+                data: body
+            });
+
+            console.log(response.data);
+            return {data: response.data};
+        } catch(error){
+            console.log(error);
+            throw error;
+        }
+    }
+});
+
+export {api};
