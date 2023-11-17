f4134b5ca72c7e07d40f24058fa6225dc7b7463d
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-17 19:00:27 +0545

    NavBar component updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function NavBar(){
     return <>
-        <Link to='/' className="link">Create</Link>
+        <Link to='/create' className="link">Create</Link>
         <Link to='/read' className="link">Read</Link>
         <Link to='/' className="link">Update</Link>
         <Link to='/' className="link">Delete</Link>
