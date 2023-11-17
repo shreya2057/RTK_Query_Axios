f42e78a1a79bad4f70895b0dfca2295a59ddf6b4
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-18 00:35:13 +0545

    Delete screen updated

--- src/screen/Delete.jsx
+++ src/screen/Delete.jsx
@@ -1,8 +1,19 @@
 import NavBar from "../components/NavBar";
-import { useGetAllDataQuery } from "../redux/api/api"
+import { useDeleteDataMutation, useGetAllDataQuery } from "../redux/api/api"
 
 function Delete(){
     const { isLoading, isError, data, isSuccess } = useGetAllDataQuery({});
+    const [delete_data] = useDeleteDataMutation();
+    const handle_delete = async(id)=>{
+        try{
+            await delete_data(id);
+            if(isSuccess){
+                alert("Deleted")
+            }
+        }catch(error){
+            console.log(error);
+        }
+    };
     return <>
         <NavBar/>
         <h3>Delete Posts</h3>
@@ -16,7 +27,7 @@ function Delete(){
                 <button 
                     className="cards"
                     onClick={()=>{
-                        
+                        handle_delete(item.id)
                     }}
                 >
                     <div><b>{item.title}</b></div>
