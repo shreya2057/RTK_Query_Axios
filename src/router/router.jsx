37a80dd0c0b101bb4eba45dcb68cab809303fcaf
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-17 18:55:57 +0545

    Router updated

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -5,13 +5,14 @@ import {
 } from "react-router-dom";
 import App from "../App";
 import Read from "../screen/Read";
+import Create from "../screen/Create";
 
 function AppRouter(){
     return <Router>
         <Routes>
             <Route path="/" element={<App/>}/>
             <Route path="read" element={<Read/>}/>
-            {/* <Route path="create" element={<App/>}/> */}
+            <Route path="create" element={<Create/>}/>
             {/* <Route path="update" element={<App/>}/> */}
             {/* <Route path="delete" element={<App/>}/> */}
         </Routes>
