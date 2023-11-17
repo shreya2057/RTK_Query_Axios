426f416926c59c1b83abdc28759bd9b01ad1c3d3
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-17 21:58:36 +0545

    Changing post screen updated

--- src/screen/ChangeData.jsx
+++ src/screen/ChangeData.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 function ChangeData(){
     const location = useLocation();
     const post = location.state;
+    const [body, setBody] = useState("");
     return <>
         <input  
             type='text'
             disabled
             value={post.post_title}
         />
+        <br/>
+        <br/>
+        <input 
+            placeholder='Enter the body' 
+            type='text'
+            onChange={(event)=>setBody(event.target.value)}
+            value={body}
+        />
+        <br/>
+        <br/>
+        <button 
+            onClick={(event)=>{change_submit(event)}}
+        >
+            Add
+        </button>
     </>
 }
 
