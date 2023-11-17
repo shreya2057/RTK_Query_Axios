7186681ec0534a2885f5a1e2113b5df4b876dd6c
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-18 00:16:39 +0545

    Delete screen updated

--- src/screen/Delete.jsx
+++ src/screen/Delete.jsx
@@ -1,9 +1,33 @@
 import NavBar from "../components/NavBar";
+import { useGetAllDataQuery } from "../redux/api/api"
 
 function Delete(){
+    const { isLoading, isError, data, isSuccess } = useGetAllDataQuery({});
     return <>
         <NavBar/>
         <h3>Delete Posts</h3>
+        {
+            isLoading &&
+            <div>Loading...</div>
+        }
+        {
+            isSuccess &&
+            data.map((item, index)=><div key={index}>
+                <button 
+                    className="cards"
+                    onClick={()=>{
+                        
+                    }}
+                >
+                    <div><b>{item.title}</b></div>
+                    <div>{item.body}</div>
+                </button>
+            </div>)
+        }
+        {
+            isError && 
+            <div>Error</div>
+        }
     </>
 }
 
