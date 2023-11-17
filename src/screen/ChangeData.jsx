4995ef001f42f5765e8ede3fae6811de69b49aa0
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-17 21:36:19 +0545

    Changing post screen

--- src/screen/ChangeData.jsx
+++ src/screen/ChangeData.jsx
@@ -3,8 +3,13 @@ import { useLocation } from "react-router-dom";
 function ChangeData(){
     const location = useLocation();
     const post = location.state;
-    console.log(post)
     return <>
+        <input 
+            placeholder='Enter the body' 
+            type='text'
+            // onChange={(event)=>setBody(event.target.value)}
+            // value={body}
+        />
         <div>{post.post_id}</div>
     </>
 }
