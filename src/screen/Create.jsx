e7af84d6e5e60a3f3a256dbef8deb7bfdc92de33
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-17 19:41:29 +0545

    Create post screen completed

--- src/screen/Create.jsx
+++ src/screen/Create.jsx
@@ -1,6 +1,55 @@
+import NavBar from "../components/NavBar"; 
+import { useState } from "react";
+import { usePostDataMutation } from "../redux/api/api";
+
 function Create(){
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [submit_post, {isLoading, isError, isSuccess}] = usePostDataMutation();
+    const posts = {
+        title: title,
+        body: body
+    };
+    const post_submit=(event)=>{
+        event.preventDefault();
+        try{
+            submit_post(posts)
+            if(isLoading){
+                console.log("Loading...")
+            }
+            if(isSuccess){
+                console.log("Success...")
+            }
+        }catch(error){
+            console.log(error)
+        }
+    }
     return <>
-        <h1>Create posts</h1>
+        <NavBar/>
+        <h3>Create posts</h3>
+        <form>
+        <input 
+          placeholder='Enter the title' 
+          type='text'
+          onChange={(event)=>setTitle(event.target.value)}
+          value={title}
+        />
+        <br/>
+        <br/>
+        <input 
+          placeholder='Enter the body' 
+          type='text'
+          onChange={(event)=>setBody(event.target.value)}
+          value={body}
+        />
+        <br/>
+        <br/>
+        <button 
+            onClick={(event)=>{post_submit(event)}}
+        >
+            Add
+        </button>
+      </form>
     </>
 }
 
