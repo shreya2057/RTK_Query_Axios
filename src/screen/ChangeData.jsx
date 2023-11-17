1c82821b45c8bc4c1a696f7e0b8450bcfec4b9f1
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-17 23:33:48 +0545

    Changing post screen update

--- src/screen/ChangeData.jsx
+++ src/screen/ChangeData.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { useUpdateDataMutation } from "../redux/api/api";
+import NavBar from "../components/NavBar";
 
 function ChangeData(){
     const location = useLocation();
+    const [update_data,{isLoading, isError, isSuccess}] = useUpdateDataMutation()
     const post = location.state;
     const [body, setBody] = useState("");
-    const change_submit = (event, id, body)=>{
+    const change_submit = async (event, id, body)=>{
         event.preventDefault();
-        const data = {
+        const posts = {
             body: body
         };
         try{
-
+            await update_data({id: id,posts});
+            if(isSuccess){
+                setBody("")
+            }
         }catch(error){
-            
+            console.log(error)
         }
     }
     return <>
+        <NavBar/>
+        <br/>
+        <br/>
         <input  
             type='text'
             disabled
