04d0279f9a29dcd966adc847ffbefd4f6f6f9a8e
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-11-18 00:03:26 +0545

    Router updated

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -8,6 +8,7 @@ import Read from "../screen/Read";
 import Create from "../screen/Create";
 import Update from "../screen/Update";
 import ChangeData from "../screen/ChangeData";
+import Delete from "../screen/Delete";
 
 function AppRouter(){
     return <Router>
@@ -17,6 +18,7 @@ function AppRouter(){
             <Route path="create" element={<Create/>}/>
             <Route path="update" element={<Update/>}/>
             <Route path="change" element={<ChangeData/>}/>
+            <Route path="delete" element={<Delete/>}/>
         </Routes>
     </Router>
 }
